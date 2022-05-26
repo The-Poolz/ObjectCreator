54835ea7df39102be8439a30401d4e25ce71922d
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2022-05-26 10:39:06 +0300

    init

---
+++ ObjectCreator/HexFormatter.cs
@@ -0,0 +1,14 @@
+﻿namespace ObjectCreator
+{
+    public static class HexFormatter
+    {
+        public static T FormatHexToJson<T>(string hex) where T : class
+        {
+            throw new NotImplementedException();
+        }
+        public static List<string> Split(string hex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
